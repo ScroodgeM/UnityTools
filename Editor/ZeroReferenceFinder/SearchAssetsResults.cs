4e5c1d56556a310418dfd864d0bfc355c5134aea
pavel vladimirov <p_vladimirov@lesta.group>
2023-01-03 17:16:04 +0300

    zero reference finder

---
+++ Editor/ZeroReferenceFinder/SearchAssetsResults.cs
@@ -0,0 +1,128 @@
+﻿
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityTools.Editor
+{
+    public class SearchAssetsResults
+    {
+        private readonly string header = string.Empty;
+        private readonly bool showDeleteButton = false;
+        private string filter = string.Empty;
+        private readonly Dictionary<string, List<string>> searchResults = new Dictionary<string, List<string>>();
+
+        public SearchAssetsResults(string header, HashSet<string> results, bool showDeleteButton)
+        {
+            this.header = header;
+            this.showDeleteButton = showDeleteButton;
+
+            foreach (string result in results)
+            {
+                string extension;
+
+                if (result.StartsWith("Assets/Resources/") == true)
+                {
+                    extension = "[Resources]";
+                }
+                else
+                {
+                    extension = System.IO.Path.GetExtension(result).ToLower();
+
+                    if (string.IsNullOrEmpty(extension) == true)
+                    {
+                        extension = "<empty>";
+                    }
+                }
+
+                if (searchResults.ContainsKey(extension) == false)
+                {
+                    searchResults.Add(extension, new List<string>());
+                }
+
+                searchResults[extension].Add(result);
+            }
+        }
+
+        public void Show()
+        {
+            if (searchResults != null)
+            {
+                GUILayout.Label(header);
+                GUILayout.Label("total results: " + searchResults.Count);
+
+                if (showDeleteButton == true)
+                {
+                    GUI.color = Color.red;
+                    if (GUILayout.Button("DELETE ALL ASSETS LISTED BELOW") == true)
+                    {
+                        foreach (KeyValuePair<string, List<string>> extension in searchResults)
+                        {
+                            foreach (string assetPath in extension.Value)
+                            {
+                                AssetDatabase.DeleteAsset(assetPath);
+                            }
+                        }
+                        searchResults.Clear();
+                    }
+                    GUI.color = Color.white;
+                }
+
+                GUILayout.Space(10f);
+
+                int counter = 0;
+                const int columns = 5;
+                EditorGUILayout.BeginHorizontal();
+                {
+                    foreach (KeyValuePair<string, List<string>> extension in searchResults)
+                    {
+                        GUI.color = filter == extension.Key ? Color.green : Color.white;
+                        if (GUILayout.Button($"{extension.Key} [{extension.Value.Count}]", GUILayout.Width(100f)))
+                        {
+                            filter = extension.Key;
+                        }
+
+                        if (GUILayout.Button("X", GUILayout.Width(20f)))
+                        {
+                            searchResults.Remove(extension.Key);
+                            break;
+                        }
+
+                        counter++;
+                        if (counter % columns == 0)
+                        {
+                            // new line
+                            EditorGUILayout.EndHorizontal();
+                            EditorGUILayout.BeginHorizontal();
+                        }
+                    }
+                    GUI.color = Color.white;
+                }
+                EditorGUILayout.EndHorizontal();
+
+                if (string.IsNullOrEmpty(filter) == false && searchResults.ContainsKey(filter) == true)
+                {
+                    GUILayout.Space(10f);
+
+                    foreach (string result in searchResults[filter])
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        if (GUILayout.Button(">>>", GUILayout.Width(40f)))
+                        {
+                            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(result));
+                        }
+                        if (GUILayout.Button("X", GUILayout.Width(40f)))
+                        {
+                            AssetDatabase.DeleteAsset(result);
+                            searchResults[filter].Remove(result);
+                            AssetDatabase.Refresh();
+                            break;
+                        }
+                        GUILayout.Label(result);
+                        EditorGUILayout.EndHorizontal();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Editor/ZeroReferenceFinder/ZeroReferenceFinder.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityTools.Editor
+{
+    public class ZeroReferenceFinder : EditorWindow
+    {
+        [MenuItem(nameof(UnityTools) + "/Zero Reference Finder")]
+        private static void Init()
+        {
+            ZeroReferenceFinder window = GetWindow(typeof(ZeroReferenceFinder)) as ZeroReferenceFinder;
+            window.titleContent = new GUIContent("Zero Reference Finder Window");
+            window.searchAssetsResults = null;
+            window.Show();
+        }
+
+        private string searchPathFilter;
+        private SearchAssetsResults searchAssetsResults;
+        private Vector2 scrollPosition;
+
+        private static readonly char[] trimChars = new char[] { ' ', '-' };
+
+        private void OnGUI()
+        {
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            searchPathFilter = EditorGUILayout.TextField("Path Filter", searchPathFilter);
+
+            if (GUILayout.Button("Search unused assets"))
+            {
+                SearchUnused();
+            }
+
+            GUILayout.Space(20f);
+
+            if (searchAssetsResults != null)
+            {
+                searchAssetsResults.Show();
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        private void SearchUnused()
+        {
+            try
+            {
+                // get all known assets
+                HashSet<string> allUnusedPaths = new HashSet<string>();
+                HashSet<string> allAssetPaths = new HashSet<string>();
+                foreach (string guid in AssetDatabase.FindAssets("t:Object"))
+                {
+                    string path = AssetDatabase.GUIDToAssetPath(guid);
+
+                    allAssetPaths.Add(path);
+
+                    if (string.IsNullOrEmpty(searchPathFilter) == false && path.ToLower().Contains(searchPathFilter.ToLower()) == false)
+                    {
+                        continue;
+                    }
+
+                    if (AssetDatabase.LoadMainAssetAtPath(path) is DefaultAsset)
+                    {
+                        continue;
+                    }
+
+                    if (path.StartsWith("Packages/"))
+                    {
+                        continue;
+                    }
+
+                    allUnusedPaths.Add(path);
+                }
+
+                Debug.Log("total assets found: " + allUnusedPaths.Count);
+
+                // for each asset get deps
+                int counter = 0;
+                foreach (string assetPath in allAssetPaths)
+                {
+                    string progress = $"Searching... {counter + 1} of {allAssetPaths.Count} ({allUnusedPaths.Count} w/o dependency)";
+
+                    if (EditorUtility.DisplayCancelableProgressBar("Searching...", progress, (float)counter / (float)allAssetPaths.Count))
+                    {
+                        searchAssetsResults = null;
+                        throw new OperationCanceledException();
+                    }
+
+                    foreach (string dependency in AssetDatabase.GetDependencies(assetPath, false))
+                    {
+                        // don't count dependency on itself
+                        if (dependency != assetPath)
+                        {
+                            allUnusedPaths.Remove(dependency);
+                        }
+                    }
+
+                    counter++;
+                }
+
+                Debug.Log("total w/o dependency: " + allUnusedPaths.Count);
+
+                searchAssetsResults = new SearchAssetsResults("assets that not used directly", allUnusedPaths, true);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                searchAssetsResults = null;
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
+    }
+}
