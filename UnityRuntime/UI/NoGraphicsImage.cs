c9f18cd73914593586bb1c93d7227b91f9c10b4f
pavel vladimirov <p_vladimirov@lesta.group>
2023-01-06 00:22:17 +0300

    no graphics image

---
+++ UnityRuntime/UI/NoGraphicsImage.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UnityTools.UnityRuntime.UI
+{
+    public class NoGraphicsImage : MaskableGraphic, IEventSystemHandler
+    {
+        public override void SetMaterialDirty() { return; }
+        public override void SetVerticesDirty() { return; }
+
+        protected override void OnPopulateMesh(VertexHelper toFill)
+        {
+            toFill.Clear();
+        }
+    }
+}
