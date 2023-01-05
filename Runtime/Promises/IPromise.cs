bf8d0485d48e184b66f6c095829586c42a9ed897
pavel vladimirov <p_vladimirov@lesta.group>
2023-01-05 16:08:47 +0300

    promises

---
+++ Runtime/Promises/IPromise.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace UnityTools.Runtime.Promises
+{
+    public interface IPromise
+    {
+        IPromise Done(Action callback);
+        IPromise Fail(Action<Exception> callback);
+        IPromise Always(Action callback);
+        IPromise Then(Func<IPromise> next);
+    }
+
+    public interface IPromise<T> : IPromise
+    {
+        IPromise<T> Done(Action<T> callback);
+    }
+}
