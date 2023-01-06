ff8e370c3a06baee577810b8b245d486c1be023f
pavel vladimirov <p_vladimirov@lesta.group>
2023-01-06 03:55:02 +0300

    stateful event for unity

---
+++ UnityRuntime/StatefulEvent/StatefulEventForUnity.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityTools.Runtime.StatefulEvent;
+
+namespace UnityTools.UnityRuntime.StatefulEvent
+{
+    public static class StatefulEventForUnity
+    {
+        public static StatefulEventInt<Vector2> Create(Vector2 defaultValue)
+        {
+            return new StatefulEventInt<Vector2>(defaultValue, (a, b) => a == b);
+        }
+        public static StatefulEventInt<Vector3> Create(Vector3 defaultValue)
+        {
+            return new StatefulEventInt<Vector3>(defaultValue, (a, b) => a == b);
+        }
+    }
+}
