72be762cc015aee8975dce815bd58d25ba7bf031
pavel vladimirov <p_vladimirov@lesta.group>
2023-01-18 02:02:51 +0300

    new types support

--- UnityRuntime/StatefulEvent/StatefulEventForUnity.cs
+++ UnityRuntime/StatefulEvent/StatefulEventForUnity.cs
@@ -6,6 +6,14 @@ namespace UnityTools.UnityRuntime.StatefulEvent
 {
     public static class StatefulEventForUnity
     {
+        public static StatefulEventInt<Vector2Int> Create(Vector2 defaultValue)
+        {
+            return new StatefulEventInt<Vector2Int>(defaultValue, (a, b) => a == b);
+        }
+        public static StatefulEventInt<Vector3Int> Create(Vector3 defaultValue)
+        {
+            return new StatefulEventInt<Vector3Int>(defaultValue, (a, b) => a == b);
+        }
         public static StatefulEventInt<Vector2> Create(Vector2 defaultValue)
         {
             return new StatefulEventInt<Vector2>(defaultValue, (a, b) => a == b);
