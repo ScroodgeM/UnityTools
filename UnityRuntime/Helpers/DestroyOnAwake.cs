7fcf3079ca615600431e55417a0ede7a7cb0dbb0
pavel vladimirov <p_vladimirov@lesta.group>
2023-01-23 00:50:57 +0300

    + destroy on awake

---
+++ UnityRuntime/Helpers/DestroyOnAwake.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+namespace UnityTools.UnityRuntime.Helpers
+{
+    public class DestroyOnAwake : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
