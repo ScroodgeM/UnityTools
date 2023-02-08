5e6bae45a3c0c89e69ed3e1b75c1b01aec79171d
pavel vladimirov <p_vladimirov@lesta.group>
2023-02-08 14:39:18 +0300

    +lazy

---
+++ Runtime/Lazy.cs
@@ -0,0 +1,31 @@
+﻿//this empty line for UTF-8 BOM header
+using System;
+
+namespace UnityTools.Runtime
+{
+    public class Lazy<T>
+    {
+        private readonly Func<T> instanceConstructor;
+        private readonly Action<T> instanceInitializer;
+
+        private T instance;
+
+        public T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = instanceConstructor();
+
+                    if (instanceInitializer != null)
+                    {
+                        instanceInitializer(instance);
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
