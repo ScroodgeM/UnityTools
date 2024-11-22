afe78ec0fe9b64481a62230faefd9acb54b4278e
pv <pv>
2024-11-22 10:21:10 +0300

    cache for links manager

--- UnityRuntime/Links/LinkDataManager.cs
+++ UnityRuntime/Links/LinkDataManager.cs
@@ -2,24 +2,45 @@
 
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
 using UnityTools.Runtime.Links;
 
 namespace UnityTools.UnityRuntime.Links
 {
     public class LinkDataManager<TL, TD> where TL : LinkBase where TD : UnityEngine.Object
     {
+        private readonly bool useCache;
         private readonly Dictionary<TL, TD> cache = new Dictionary<TL, TD>();
+
+        public LinkDataManager(bool useCache = true)
+        {
+            this.useCache = useCache;
+        }
 
         public TD GetByLink(TL link)
         {
             if (cache.TryGetValue(link, out TD cachedObject) == false)
             {
-                cachedObject = Resources.Load<TD>(Path.Combine(LinkBase.GetPathForAssetInsideResources<TD>(), link.LinkedObjectId));
-                cache.Add(link, cachedObject);
+                cachedObject = UnityEngine.Resources.Load<TD>(Path.Combine(LinkBase.GetPathForAssetInsideResources<TD>(), link.LinkedObjectId));
+                if (useCache == true)
+                {
+                    cache.Add(link, cachedObject);
+                }
             }
 
             return cachedObject;
+        }
+
+        public void ClearCachedObjects()
+        {
+            foreach (TD dataObject in cache.Values)
+            {
+                if (dataObject != null)
+                {
+                    UnityEngine.Resources.UnloadAsset(dataObject);
+                }
+            }
+
+            cache.Clear();
         }
     }
 }
