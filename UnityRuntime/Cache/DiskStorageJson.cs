42d7c12ca6fc5864fb7829973453ae6e0e9fca96
Pavel Vladimirov <p_vladimirov@lesta.group>
2025-08-03 00:06:34 +0300

    json pretty print for editor and development builds

--- UnityRuntime/Cache/DiskStorageJson.cs
+++ UnityRuntime/Cache/DiskStorageJson.cs
@@ -13,6 +13,14 @@ namespace UnityTools.UnityRuntime.Cache
 
         protected override T ReadDataFromDisk(string filePath) => JsonUtility.FromJson<T>(File.ReadAllText(filePath));
 
-        protected override void WriteDataToDisk(string filePath, T data) => File.WriteAllText(filePath, JsonUtility.ToJson(data));
+        protected override void WriteDataToDisk(string filePath, T data)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            const bool prettyPrint = true;
+#else
+            const bool prettyPrint = false;
+#endif
+            File.WriteAllText(filePath, JsonUtility.ToJson(data, prettyPrint));
+        }
     }
 }
