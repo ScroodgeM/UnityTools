51d950bd5f776d5de3fc259d6eeba9de3c50133d
Pavel Vladimirov <p_vladimirov@lesta.group>
2025-08-03 00:12:59 +0300

    open cache storage folder via editor menu item

---
+++ Editor/Cache/CacheEditorHelper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityTools.Editor.Cache
+{
+    public static class CacheEditorHelper
+    {
+        [MenuItem(nameof(UnityTools) + "/Open cache storage folder")]
+        public static void AnalyzeCode()
+        {
+            EditorUtility.RevealInFinder(System.IO.Path.Combine(Application.persistentDataPath, "*"));
+        }
+    }
+}
