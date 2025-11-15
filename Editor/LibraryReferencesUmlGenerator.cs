636e46fbc87d498754d07603a019a760327ef385
Pavel Vladimirov <p_vladimirov@lesta.group>
2025-11-15 13:36:29 +0300

    better colored UML

--- Editor/LibraryReferencesUmlGenerator.cs
+++ Editor/LibraryReferencesUmlGenerator.cs
@@ -13,6 +13,7 @@ namespace UnityTools.Editor
 
         private enum AssemblyType : byte
         {
+            Unknown = 0,
             Self = 3,
             Unity = 10,
             ThirdParty = 20,
@@ -23,7 +24,7 @@ namespace UnityTools.Editor
         }
 
         [Serializable]
-        private struct Config
+        private class Config
         {
             [Serializable]
             public struct ColorHighlight
@@ -36,12 +37,16 @@ namespace UnityTools.Editor
             public string[] thirdPartyAssemblies;
             public string[] thirdPartyEditorAssemblies;
             public string[] projectSharedAssemblies;
+            public string[] projectAssemblies;
             public string[] projectEditorAssemblies;
+
             public string[] hiddenAssemblies;
+
+            public string[] outputUnusedAssemblies;
+            public string[] outputMissingAssemblies;
+
             public ColorHighlight[] colorHighlights;
         }
-
-        private static Config config;
 
         private static readonly string[] selfAssemblies = new string[]
         {
@@ -62,7 +67,7 @@ namespace UnityTools.Editor
         [UnityEditor.MenuItem(nameof(UnityTools) + "/Generate UML with assembly references")]
         private static void GenerateUML()
         {
-            ReadConfig();
+            Config config = ReadConfig();
 
             System.Text.StringBuilder umlDocument = new System.Text.StringBuilder();
 
@@ -74,7 +79,7 @@ namespace UnityTools.Editor
 
             umlDocument.AppendLine();
 
-            CreateUml(System.AppDomain.CurrentDomain.GetAssemblies(), ref umlDocument);
+            config.CreateUml(AppDomain.CurrentDomain.GetAssemblies(), ref umlDocument);
 
             umlDocument.AppendLine();
 
@@ -82,23 +87,34 @@ namespace UnityTools.Editor
 
             File.WriteAllText(GetPathToUMLFile(), umlDocument.ToString());
 
+            WriteConfig(config);
+
             UnityEngine.Debug.Log("Generation success");
         }
 
-        private static void ReadConfig()
+        private static Config ReadConfig()
         {
-            string pathToConfig = Path.Combine(Application.dataPath, "uml_generator_config.json");
-
-            config = File.Exists(pathToConfig) ? JsonUtility.FromJson<Config>(File.ReadAllText(pathToConfig)) : default;
-
-            File.WriteAllText(pathToConfig, JsonUtility.ToJson(config, true));
+            return File.Exists(GetPathToConfig()) ? JsonUtility.FromJson<Config>(File.ReadAllText(GetPathToConfig())) : default;
         }
 
-        private static void CreateUml(Assembly[] assemblies, ref System.Text.StringBuilder umlDocument)
+        private static void WriteConfig(Config config)
+        {
+            File.WriteAllText(GetPathToConfig(), JsonUtility.ToJson(config, true));
+        }
+
+        private static void CreateUml(this Config config, Assembly[] assemblies, ref System.Text.StringBuilder umlDocument)
         {
             List<Assembly> assembliesSorted = new List<Assembly>(assemblies);
 
-            assembliesSorted.Sort((a, b) => a.FormatAssemblyName().CompareTo(b.FormatAssemblyName()));
+            assembliesSorted.Sort((a, b) => config.FormatAssemblyName(a).CompareTo(config.FormatAssemblyName(b)));
+
+            HashSet<string> unusedAssemblies = new HashSet<string>();
+            HashSet<string> missingAssemblies = new HashSet<string>();
+
+            foreach (string mentionedAssembly in config.GetAllMentionedAssemblies())
+            {
+                unusedAssemblies.Add(mentionedAssembly);
+            }
 
             for (int i = 0; i < assembliesSorted.Count; i++)
             {
@@ -106,9 +122,9 @@ namespace UnityTools.Editor
 
                 bool hasEngineReferences = assembly.HasEngineReferences();
 
-                umlDocument.AppendLine($"class {assembly.FormatAssemblyName()} {assembly.GetAssemblyColor(hasEngineReferences)} {{");
+                umlDocument.AppendLine($"class {config.FormatAssemblyName(assembly)} {config.GetAssemblyColor(assembly, hasEngineReferences)} {{");
 
-                umlDocument.AppendLine(GetAssemblyBody(assembly));
+                umlDocument.AppendLine(config.GetAssemblyBody(assembly));
 
                 umlDocument.AppendLine("}");
 
@@ -121,22 +137,31 @@ namespace UnityTools.Editor
                         assembliesSorted.Add(CreateAssemblyFromName(reference));
                     }
 
-                    if (reference.GetAssemblyType() >= assembly.GetAssemblyType())
+                    if (config.GetAssemblyType(reference) >= config.GetAssemblyType(assembly))
                     {
-                        umlDocument.AppendLine($"{reference.FormatAssemblyName()} <-- {assembly.FormatAssemblyName()}");
+                        umlDocument.AppendLine($"{config.FormatAssemblyName(reference)} <-- {config.FormatAssemblyName(assembly)}");
                     }
                 }
 
                 umlDocument.AppendLine();
+
+                string assemblyName = assembly.GetName().Name;
+                if (unusedAssemblies.Remove(assemblyName) == false)
+                {
+                    missingAssemblies.Add(assemblyName);
+                }
             }
 
             if (config.hiddenAssemblies != null)
             {
                 foreach (string hiddenAssembly in config.hiddenAssemblies)
                 {
-                    umlDocument.AppendLine("remove " + hiddenAssembly.FormatAssemblyName());
+                    umlDocument.AppendLine("remove " + config.FormatAssemblyName(hiddenAssembly));
                 }
             }
+
+            config.outputUnusedAssemblies = new List<string>(unusedAssemblies).ToArray();
+            config.outputMissingAssemblies = new List<string>(missingAssemblies).ToArray();
         }
 
         private static Assembly CreateAssemblyFromName(AssemblyName assemblyName)
@@ -145,45 +170,46 @@ namespace UnityTools.Editor
                 .DefineDynamicAssembly(assemblyName, System.Reflection.Emit.AssemblyBuilderAccess.ReflectionOnly);
         }
 
-        private static string FormatAssemblyName(this Assembly assembly) => assembly.GetName().FormatAssemblyName();
+        private static string FormatAssemblyName(this Config config, Assembly assembly) => config.FormatAssemblyName(assembly.GetName());
 
-        private static string FormatAssemblyName(this AssemblyName assemblyName) => assemblyName.Name.FormatAssemblyName();
+        private static string FormatAssemblyName(this Config config, AssemblyName assemblyName) => config.FormatAssemblyName(assemblyName.Name);
 
-        private static string FormatAssemblyName(this string assemblyName) => $"{assemblyName.GetAssemblyType()}." + assemblyName.Replace(" ", "_").Replace("-", "_").Replace(".", "_");
+        private static string FormatAssemblyName(this Config config, string assemblyName) => $"{config.GetAssemblyType(assemblyName)}." + assemblyName.Replace(" ", "_").Replace("-", "_").Replace(".", "_");
 
-        private static AssemblyType GetAssemblyType(this Assembly assembly) => assembly.GetName().GetAssemblyType();
+        private static AssemblyType GetAssemblyType(this Config config, Assembly assembly) => config.GetAssemblyType(assembly.GetName());
 
-        private static AssemblyType GetAssemblyType(this AssemblyName assemblyName) => assemblyName.Name.GetAssemblyType();
+        private static AssemblyType GetAssemblyType(this Config config, AssemblyName assemblyName) => config.GetAssemblyType(assemblyName.Name);
 
-        private static AssemblyType GetAssemblyType(this string assemblyName)
+        private static AssemblyType GetAssemblyType(this Config config, string assemblyName)
         {
             if (assemblyName.IsInArray(config.unityAssemblies)) return AssemblyType.Unity;
             if (assemblyName.IsInArray(config.thirdPartyAssemblies)) return AssemblyType.ThirdParty;
-            if (assemblyName.IsInArray(config.projectSharedAssemblies)) return AssemblyType.ProjectShared;
             if (assemblyName.IsInArray(config.thirdPartyEditorAssemblies)) return AssemblyType.ThirdPartyEditor;
+            if (assemblyName.IsInArray(config.projectSharedAssemblies)) return AssemblyType.ProjectShared;
+            if (assemblyName.IsInArray(config.projectAssemblies)) return AssemblyType.Project;
             if (assemblyName.IsInArray(config.projectEditorAssemblies)) return AssemblyType.ProjectEditor;
             if (assemblyName.IsInArray(selfAssemblies)) return AssemblyType.Self;
-            return AssemblyType.Project;
+            return AssemblyType.Unknown;
         }
 
-        private static string GetAssemblyBody(Assembly assembly)
+        private static string GetAssemblyBody(this Config config, Assembly assembly)
         {
             bool hasEngineReferences = Array.Exists(assembly.GetReferencedAssemblies(), x => x.Name == UnityCoreModuleAssemblyName);
             string result = hasEngineReferences ? "uses Unity" : "no Unity";
 
             result += ", ";
-            result += assembly.GetAssemblyType().ToString();
+            result += config.GetAssemblyType(assembly).ToString();
 
             return result;
         }
 
-        private static string GetAssemblyColor(this Assembly assembly, bool hasEngineReferences) => assembly.GetName().GetAssemblyColor(hasEngineReferences);
+        private static string GetAssemblyColor(this Config config, Assembly assembly, bool hasEngineReferences) => config.GetAssemblyColor(assembly.GetName(), hasEngineReferences);
 
-        private static string GetAssemblyColor(this AssemblyName assemblyName, bool hasEngineReferences)
+        private static string GetAssemblyColor(this Config config, AssemblyName assemblyName, bool hasEngineReferences)
         {
             Color secondColor = hasEngineReferences ? new Color(0.20f, 0.05f, 0.05f) : new Color(0.05f, 0.20f, 0.05f);
 
-            Color firstColor = assemblyName.GetAssemblyType().GetColor();
+            Color firstColor = config.GetAssemblyType(assemblyName).GetColor();
 
             if (config.colorHighlights != null)
             {
@@ -229,6 +255,20 @@ namespace UnityTools.Editor
         private static bool IsInArray(this AssemblyName assemblyName, string[] searchIn) => assemblyName.Name.IsInArray(searchIn);
 
         private static bool IsInArray(this string text, string[] searchIn) => searchIn != null && Array.Exists(searchIn, x => x == text);
+
+        private static List<string> GetAllMentionedAssemblies(this Config config)
+        {
+            List<string> allMentionedAssemblies = new List<string>();
+            if (config.unityAssemblies != null) allMentionedAssemblies.AddRange(config.unityAssemblies);
+            if (config.thirdPartyAssemblies != null) allMentionedAssemblies.AddRange(config.thirdPartyAssemblies);
+            if (config.thirdPartyEditorAssemblies != null) allMentionedAssemblies.AddRange(config.thirdPartyEditorAssemblies);
+            if (config.projectSharedAssemblies != null) allMentionedAssemblies.AddRange(config.projectSharedAssemblies);
+            if (config.projectAssemblies != null) allMentionedAssemblies.AddRange(config.projectAssemblies);
+            if (config.projectEditorAssemblies != null) allMentionedAssemblies.AddRange(config.projectEditorAssemblies);
+            return allMentionedAssemblies;
+        }
+
+        private static string GetPathToConfig() => Path.Combine(Application.dataPath, "uml_generator_config.json");
 
         private static string GetPathToUMLFile() => Path.Combine(Application.dataPath, "../architecture.plantuml");
     }
